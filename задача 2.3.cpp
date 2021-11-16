baeb7904112438af1251c8c3879b19c526a0414f
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2021-11-16 17:30:04 +0500

    Add files via upload

---
+++ задача 2.3.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	int t, sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		t = 1;
+		for (int j = i; j <= i * 2; j++)
+		{
+			t *= j;
+		}
+		sum += t;
+	}
+	cout << "Итог - " << sum;
+	return 0;
+}
