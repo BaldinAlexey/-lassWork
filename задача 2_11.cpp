eb03d2075aabae7d5b1b5350daeecc6a322a279e
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2021-11-23 22:33:55 +0500

    Add files via upload

---
+++ задача 2_11.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n, i, tmp;
+    bool answerFound = false;
+
+    cout << "Введите длину последовательности:\n";
+    cin >> n;
+    i = 1;
+
+    while (i <= n && !answerFound)
+    {
+        cin >> tmp;
+        if (tmp == 0)
+        {
+            i++;
+        }
+        else
+        {
+            answerFound = true;
+        }
+    }
+
+    if (tmp > 0)
+    {
+        cout << "Сначала ввели положительное число";
+    }
+    else
+    {
+        cout << "Сначала ввели отрицательное число";
+    }
+    return 0;
+}
