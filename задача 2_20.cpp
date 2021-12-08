d4f841d3b0001fe886a2528469d764512ab70ec4
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2021-12-08 13:34:01 +0500

    Add files via upload

---
+++ задача 2_20.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float n, z, a;
+	int i = 1;
+	bool flag = false;
+	cin >> n >> z;
+	while (i <= n && !flag)
+	{
+		a = sin(n + i / n);
+		a = round(a * 10) / 10; // для удобства проверки
+			if (a == z)
+			{
+				flag = true;
+			}
+			else { i++; }
+	}
+	if (flag)
+	{
+		cout << "Элемент " << z << "найден. Его номер: " << i << endl;
+	}
+	else
+	{
+		cout << "Элемент не найден" << endl;
+	}
+	return 0;
+}
+
---
+++ задача 2_22.cpp
@@ -0,0 +1,29 @@
+﻿#include <cmath>
+# include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float n, a, max;
+	int i = 2, posFirst = 1, posLast = 1;
+	cin >> n;
+	max = sin(n + 1 / n);
+	while (i <= n)
+	{
+		a = sin(n + i / n);
+		if (a > max)
+		{
+			max = a;
+			posFirst = i;
+			posLast = i;
+		}
+		else if (a == max)
+		{posLast = i; }
+		i++;
+	}
+	cout << "Max = " << max << endl;
+	cout << "Первый элемент с этим значением = " << posFirst << endl;
+	cout << "Последний элемент с этим значением = " << posLast << endl;
+	return 0;
+}
+
