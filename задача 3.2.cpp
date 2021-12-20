6c543d2d5f9daf1901f5fbbe10d45254c6a8f823
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2021-12-20 19:03:06 +0500

    Add files via upload

---
+++ задача 3.2.cpp
@@ -0,0 +1,39 @@
+﻿// Сортировка выбором 
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n = 5;
+    int tmp, min_index; //Индекс минимального элемента
+    int arr[5] = { 5,2,0,4,3 };
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        //swap
+        tmp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = tmp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
